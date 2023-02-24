1b1db1888465dc1e38a3dab629bef2ad42b0d296
codewithanky <ankitanandbp@gmail.com>
2023-02-24 19:12:10 +0530

    secod commit

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -13,15 +13,7 @@ export  function Navbar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton
-            size="medium"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
+          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
            Tourism
           </Typography>
